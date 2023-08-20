e2b783db32557e1643b4beeda23bd57079e91e7e
Sai Maneesh <saimaneesh1115@gmail.com>
2023-08-20 14:18:34 +0530

    adding items to the cart

--- src/components/HeaderCartButton.js
+++ src/components/HeaderCartButton.js
@@ -1,11 +1,18 @@
 import styles from "./HeaderCartButton.module.css";
+import { useContext } from "react";
+import CartContext from "./Cart-Context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}></span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
